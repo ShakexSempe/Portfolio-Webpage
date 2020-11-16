fcae20bee23fa22319d4302b4167fd89c602c34b
Shakex <bshakex@gmail.com>
2020-11-16 13:11:28 +0200

    created a Portfolio Page repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio-Webpage
+Responsive Web Design Projects #5
+
+This is a Personal Portfolio page which is the last of the freeCodeCamp responsive web design projects.
+View the page on the following URL: https://shakexsempe.github.io/Portfolio-Webpage/
---
+++ js/app.js
@@ -0,0 +1,28 @@
+//Hamburger Menu
+const hamburger = document.querySelector('.header #navbar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header #navbar .nav-list ul');
+const menu_item = document.querySelectorAll('.header #navbar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+//nav transparency on scroll
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 200) {
+        header.style.backgroundColor = "#29323c";
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+//remove hamburger after 
+menu_item.forEach((item) =>{
+    item.addEventListener('click', ()=> {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
