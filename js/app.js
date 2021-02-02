dcb7a205e775e03f6a3cb812b11530141cd0c8d8
Ray-Shakex <bshakex@gmail.com>
2021-02-02 10:22:59 +0200

    added animation to header button

--- js/app.js
+++ js/app.js
@@ -61,22 +61,22 @@ function dropdown() {
   }
 /*end of skills dropdown*/  
 
-/*projects dropdown*/
-function projectsDropdown() {
-  document.getElementById("projectsDropdown").classList.toggle("show");
-}
+// /*projects dropdown*/
+// function projectsDropdown() {
+//   document.getElementById("projectsDropdown").classList.toggle("show");
+// }
 
-//Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// //Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
 /*end of projects dropdown*/
