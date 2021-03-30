f5c32cf323648a54322261be65d252ef6ddf5b2d
Ray-Shakex <bshakex@gmail.com>
2021-03-30 21:40:52 +0200

    deleted redundant images and icons

--- js/app.js
+++ js/app.js
@@ -70,24 +70,3 @@ $(function(){
       theme: "dark-thin"
   });
 });
-/*end of scrollbar*/
-
-// /*projects dropdown*/
-// function projectsDropdown() {
-//   document.getElementById("projectsDropdown").classList.toggle("show");
-// }
-
-// //Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
-/*end of projects dropdown*/
