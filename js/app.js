f0e6ff66884cf7cd423141a939b80b0598ee252a
Ray-Shakex <bshakex@gmail.com>
2021-04-28 16:49:10 +0200

    fixed home button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 const linksContainer = document.querySelector('.links-container');
 const scrollLinks = document.querySelectorAll('.scroll-link');
 const navbar = document.getElementById('navbar');
+
 scrollLinks.forEach(link => {
   console.log(link)
     //prevent default
@@ -56,12 +57,21 @@ hamburger.addEventListener('click', () => {
 
 //nav transparency on scroll
 document.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 100) {
+      var scroll_position = window.scrollY;
+      const topLink = document.querySelector('.top-link');
+      const scrollHeight = window.pageYOffset;
+
+      if (scroll_position > 100) {
         header.style.backgroundColor = "#29323c";
-    } else {
+      } else {
         header.style.backgroundColor = 'transparent';
-    }
+      };
+
+      if (scrollHeight > 400) {
+        topLink.classList.add('show-link');
+      } else {
+        topLink.classList.remove('show-link');
+      }
 });
 
 //remove hamburger after 
