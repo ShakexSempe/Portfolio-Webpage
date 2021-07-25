43d0421bdddc470a6a38aeb20e88542404b96af0
Ray-Shakex <bshakex@gmail.com>
2021-07-25 12:13:15 +0200

    sidebar avi event listener

--- js/app.js
+++ js/app.js
@@ -5,25 +5,25 @@ const scrollLinks = document.querySelectorAll('.scroll-link');
 const navbar = document.getElementById('navbar');
 
 scrollLinks.forEach(link => {
-    //prevent default
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        //navigate to specific spot
-        const id = e.currentTarget.getAttribute('href').slice(1);
-        const element = document.getElementById(id);
-        //calculate heights
-        const navHeight = navbar.getBoundingClientRect().height;
-        const containerHeight = linksContainer.getBoundingClientRect().height;
-        const fixedNav = navbar.classList.contains('fixed-nav');
+  //prevent default
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    //navigate to specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    //calculate heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
 
-        let position = element.offsetTop - navHeight;
-        
-        
-        window.scrollTo({
-            left: 0,
-            top: position,
-        });
+    let position = element.offsetTop - navHeight;
+
+
+    window.scrollTo({
+      left: 0,
+      top: position,
     });
+  });
 });
 
 /* cta scroll to about */
@@ -37,7 +37,7 @@ scrollLinks.forEach(link => {
 
 //   let position = element.offsetTop - navHeight;
 //   console.log(position)
-   
+
 //   window.scrollTo({
 //     left: 0,
 //     top: position,
@@ -53,42 +53,54 @@ const hamburger = document.querySelector('.header #navbar .nav-list .hamburger')
 const mobile_menu = document.querySelector('.header #navbar .nav-list ul');
 const menu_item = document.querySelectorAll('.header #navbar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const avi = document.querySelector('.header #navbar .nav-list .nav-avi img');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
 });
 
 //nav transparency on scroll
 document.addEventListener('scroll', () => {
-      var scroll_position = window.scrollY;
-      const topLink = document.querySelector('.top-link');
-      const scrollHeight = window.pageYOffset;
+  var scroll_position = window.scrollY;
+  const topLink = document.querySelector('.top-link');
+  const scrollHeight = window.pageYOffset;
 
-      if (scroll_position > 100) {
-        header.style.backgroundColor = "#29323c";
-      } else {
-        header.style.backgroundColor = 'transparent';
-      };
+  if (scroll_position > 100) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = 'transparent';
+  };
 
-      if (scrollHeight > 700) {
-        topLink.classList.add('show-link');
-      } else {
-        topLink.classList.remove('show-link');
-      }
+  if (scrollHeight > 700) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
 });
 
-//remove hamburger after 
-menu_item.forEach((item) =>{
-    item.addEventListener('click', ()=> {
-        hamburger.classList.toggle('active');
-        mobile_menu.classList.toggle('active');
-    });
+//  remove hamburger after click 
+menu_item.forEach((item) => {
+  item.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+  });
 });
+//  end of remove hamburger after click 
 
-//progress bar
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+//  remove sidebar after click on img 
+avi.addEventListener("click", () => {
+  hamburger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+})
+//  end of remove sidebar after click on img 
+
+
+//  progress bar
+//  When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction()
+};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -96,42 +108,42 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
-/*end of progress bar*/
+/*  end of progress bar*/
 
 
 
 
-/*skills dropdown*/
-/* When the user clicks on the button,
+/*  skills dropdown*/
+/*  When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  //Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+//  Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
-/*end of skills dropdown*/  
+}
+/*  end of skills dropdown*/
 
-/*bootsrap scrollbar*/
-$(function(){
+/*  bootsrap scrollbar*/
+$(function () {
   $("#content-1").mCustomScrollbar({
-      theme: "dark-thin"
+    theme: "dark-thin"
   });
 });
-/*end of bootsrap scrollbar*/
+/*  end of bootsrap scrollbar*/
 
-/*set footer date*/
+/*  set footer date*/
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
-/*end of set footer date*/
+/*  end of set footer date*/
