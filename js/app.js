2e0b8fdbf7a27e60b04aa3fffcd8cde227140561
Ray-Shakex <bshakex@gmail.com>
2023-04-25 09:40:26 +0200

    active header link

--- js/app.js
+++ js/app.js
@@ -27,6 +27,8 @@ console.log(activePage);
 navLinks.forEach(link => {
   if(link.href.includes(`${activePage}`)){
     link.classList.add('active');
+  } else {
+    link.classList.remove('active')
   }
 });
 
